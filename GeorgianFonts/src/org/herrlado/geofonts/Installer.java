4f2741f77a9e135c71be3ea0d0493ab400264c37
herrlado@gmail.com <herrlado@gmail.com@420efb84-5ef6-8935-1a5f-f9f736657953>
2011-12-18 21:28:54 +0000

    ics suppor

--- GeorgianFonts/src/org/herrlado/geofonts/Installer.java
+++ GeorgianFonts/src/org/herrlado/geofonts/Installer.java
@@ -7,6 +7,7 @@ import java.io.InputStream;
 import java.io.LineNumberReader;
 import java.io.OutputStream;
 import java.io.StringReader;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -23,6 +24,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
@@ -35,7 +37,7 @@ import android.widget.Button;
 import android.widget.ProgressBar;
 
 public class Installer extends Activity implements OnClickListener,
-DialogInterface.OnClickListener {
+		DialogInterface.OnClickListener {
 
 	public static final String TAG = "GeoFontsInstaller";
 	public static final String DroidSansBold = "DroidSans-Bold.ttf";
@@ -45,10 +47,26 @@ DialogInterface.OnClickListener {
 	public static final String DroidSerifBoldItalic = "DroidSerif-BoldItalic.ttf";
 	public static final String DroidSerifItalic = "DroidSerif-Italic.ttf";
 	public static final String DroidSerifRegular = "DroidSerif-Regular.ttf";
+	public static final int ICS_SDK = 14;
+
+	public static final ArrayList<String> ROBOTO = new ArrayList<String>();
+
+	static {
+		ROBOTO.add("Roboto-BoldCondensedItalic.ttf");
+		ROBOTO.add("Roboto-BoldCondensed.ttf");
+		ROBOTO.add("Roboto-BoldItalic.ttf");
+		ROBOTO.add("Roboto-Bold.ttf");
+		ROBOTO.add("Roboto-CondensedItalic.ttf");
+		ROBOTO.add("Roboto-Condensed.ttf");
+		ROBOTO.add("Roboto-Italic.ttf");
+		ROBOTO.add("Roboto-Regular.ttf");
+
+	}
+
 	public static final HashMap<String, String> MD5 = new HashMap<String, String>();
 	public static final String DESTINATION = "/system/fonts";
 	public static final Pattern MOUNT_SYSTEM_PATTERN = Pattern
-	.compile("([^\\s]*)\\s.*(/system)\\s.*");
+			.compile("([^\\s]*)\\s.*(/system)\\s.*");
 
 	static {
 		MD5.put(DroidSansBold, "bd3163f7db07b82158a310efd77902ce");
@@ -62,31 +80,45 @@ DialogInterface.OnClickListener {
 
 	private SDCardMountIntentReceiver sDCardMountIntentReceiver = null;
 
-	private void alertUser(String text, DialogInterface.OnClickListener listener){
-		new AlertDialog.Builder(this)
-		.setMessage(text)
-		.setCancelable(false)
-		.setTitle(R.string.alert_warn)
-		.setIcon(android.R.drawable.ic_dialog_alert)
-		.setPositiveButton(R.string.alert_ok, listener).show();
+	private void alertUser(String text, DialogInterface.OnClickListener listener) {
+		new AlertDialog.Builder(this).setMessage(text).setCancelable(false)
+				.setTitle(R.string.alert_warn)
+				.setIcon(android.R.drawable.ic_dialog_alert)
+				.setPositiveButton(R.string.alert_ok, listener).show();
 	}
 
-	private void notifyUser(String text){
-		new AlertDialog.Builder(this)
-		.setMessage(text)
-		.setCancelable(false)
-		.setTitle(R.string.alert_info)
-		.setIcon(android.R.drawable.ic_dialog_info)
-		.setPositiveButton(R.string.alert_ok, null).show();
+	private void notifyUser(String text) {
+		new AlertDialog.Builder(this).setMessage(text).setCancelable(false)
+				.setTitle(R.string.alert_info)
+				.setIcon(android.R.drawable.ic_dialog_info)
+				.setPositiveButton(R.string.alert_ok, null).show();
 	}
 
 	private String getBackupFolder() {
 		return Environment.getExternalStorageDirectory() + "/"
-		+ getApplicationContext().getPackageName();
+				+ getApplicationContext().getPackageName();
 	}
 
 	private String getTmpFontFolder() {
 		return getBackupFolder() + "/tmp";
+	}
+
+	ArrayList<String> getFonts() {
+
+		ArrayList<String> fonts = new ArrayList<String>(MD5.keySet());
+
+		String rf = ROBOTO.get(0);
+		File roboto_font = new File("/system/fonts/" + rf);
+		int api = Build.VERSION.SDK_INT; 
+		if ( api >= ICS_SDK && roboto_font.exists() == false) {
+			return fonts;
+		}
+
+		for (String f : ROBOTO) {
+			fonts.add(f);
+		}
+
+		return fonts;
 	}
 
 	private boolean copyFonts(String sourceFolder){
@@ -101,10 +133,11 @@ DialogInterface.OnClickListener {
 			sb.append(cc).append("\n");
 
 			check(sc.su.runWaitFor(cc));
+			
 
-			for(String key : MD5.keySet()){
-				String source = sourceFolder + "/" + key;
-				String dest = DESTINATION + "/" + key;
+			for(String font : getFonts()){
+				String source = sourceFolder + "/" + font;
+				String dest = DESTINATION + "/" + font;
 				cc = "cat " + source + " > " + dest;
 				sb.append(cc).append("\n");
 				check(sc.su.runWaitFor(cc));
@@ -117,7 +150,7 @@ DialogInterface.OnClickListener {
 			sb.append(cc).append("\n");
 			check(sc.su.runWaitFor(cc));
 
-			try{
+			try {
 				cc = "mount -o remount,ro " + system + " /system";
 				sb.append(cc).append("\n");
 				check(sc.su.runWaitFor(cc));
@@ -134,7 +167,7 @@ DialogInterface.OnClickListener {
 
 	private boolean restore() {
 		File backUp = new File(getBackupFolder());
-		if(!backUp.isDirectory()){
+		if (!backUp.isDirectory()) {
 			Log.w(TAG, "backUp is not a directory");
 			return false;
 		}
@@ -177,17 +210,17 @@ DialogInterface.OnClickListener {
 		startActivity(deleteThis);
 	}
 
-	private void getPage(){
-		Uri url = Uri.parse("http://www.addictivetips.com/mobile/how-to-root-your-android-phone-device/");
+	private void getPage() {
+		Uri url = Uri
+				.parse("http://www.addictivetips.com/mobile/how-to-root-your-android-phone-device/");
 		Intent launchBrowser = new Intent(Intent.ACTION_VIEW, url);
 		startActivity(launchBrowser);
 
 	}
 
-	private boolean SDpresent(){
-		return 
-		Environment.getExternalStorageState()
-		.equals(Environment.MEDIA_MOUNTED);
+	private boolean SDpresent() {
+		return Environment.getExternalStorageState().equals(
+				Environment.MEDIA_MOUNTED);
 	}
 
 	public boolean backup() {
@@ -197,17 +230,17 @@ DialogInterface.OnClickListener {
 				|| backupFolder.canWrite() == false) {
 			Log.w(TAG,
 					backupFolder
-					+ " can not be created or it exists and is not a directory or not writable!");
+							+ " can not be created or it exists and is not a directory or not writable!");
 			return false;
 		}
 		FileInputStream fis = null;
 		FileOutputStream fos = null;
 		try {
-			for (String font : MD5.keySet()) {
+			for (String font : getFonts()) {
 				fis = new FileInputStream(new File("/system/fonts/" + font));
 				fos = new FileOutputStream(new File(backupFolder + "/" + font));
 				IOUtils.copy(fis, fos);
-			} 
+			}
 		} catch (Exception ex) {
 			notifyUser(ex.getMessage());
 			return false;
@@ -226,7 +259,7 @@ DialogInterface.OnClickListener {
 		InputStream is = null;
 		OutputStream os = null;
 		try {
-			for (String key : MD5.keySet()) {
+			for (String key : getFonts()) {
 				is = getApplicationContext().getAssets().open(key);
 				os = new FileOutputStream(new File(folder + key));
 				IOUtils.copy(is, os);
@@ -251,10 +284,9 @@ DialogInterface.OnClickListener {
 		String system = getSystemPartion();
 
 		if (system == null) {
-			Log.w(TAG,
-			"Cannot find out which partion is mounted on /system");
+			Log.w(TAG, "Cannot find out which partion is mounted on /system");
 			return false;
-		}	
+		}
 		return copyFonts(getTmpFontFolder());
 	}
 
@@ -267,7 +299,7 @@ DialogInterface.OnClickListener {
 		IntentFilter filter = new IntentFilter(Intent.ACTION_MEDIA_MOUNTED);
 		filter.addAction(Intent.ACTION_MEDIA_EJECT);
 		filter.addDataScheme("file");
-		sDCardMountIntentReceiver  = new SDCardMountIntentReceiver(this);
+		sDCardMountIntentReceiver = new SDCardMountIntentReceiver(this);
 		registerReceiver(sDCardMountIntentReceiver, filter);
 
 		Button button = (Button) findViewById(R.id.uninstall_this_app);
@@ -276,36 +308,39 @@ DialogInterface.OnClickListener {
 		ShellCommand shc = new ShellCommand();
 		if (shc.canSU(true) == false) {
 			new AlertDialog.Builder(this)
-			.setTitle(R.string.alert_warn)
-			.setMessage("This app cannot gain Super User permissions. Is your device rooted?")
-			.setIcon(android.R.drawable.ic_dialog_alert)
-			.setCancelable(false)
-			.setPositiveButton(R.string.alert_ok, null)
-			.setNegativeButton(R.string.alert_help, new DialogInterface.OnClickListener() {
+					.setTitle(R.string.alert_warn)
+					.setMessage(
+							"This app cannot gain Super User permissions. Is your device rooted?")
+					.setIcon(android.R.drawable.ic_dialog_alert)
+					.setCancelable(false)
+					.setPositiveButton(R.string.alert_ok, null)
+					.setNegativeButton(R.string.alert_help,
+							new DialogInterface.OnClickListener() {
 
-				@Override
-				public void onClick(DialogInterface dialog, int id) {
-					getPage();
-				}
-			}).show();
+								@Override
+								public void onClick(DialogInterface dialog,
+										int id) {
+									getPage();
+								}
+							}).show();
 			return;
 		}
-		if(SDpresent()){
+		if (SDpresent()) {
 			enableView();
-			//			button = (Button) findViewById(R.id.install_fonts);
-			//			button.setVisibility(View.VISIBLE);
-			//			button.setEnabled(true);
-			//			button.setOnClickListener(this);
+			// button = (Button) findViewById(R.id.install_fonts);
+			// button.setVisibility(View.VISIBLE);
+			// button.setEnabled(true);
+			// button.setOnClickListener(this);
 			//
-			//			button = (Button) findViewById(R.id.restore_fonts);
-			//			button.setVisibility(View.VISIBLE);
-			//			if(new File(getBackupFolder()).exists()){
-			//				button.setEnabled(true);
-			//			}else{
-			//				button.setEnabled(false);
-			//			}
-			//			button.setOnClickListener(this);
-		}else{
+			// button = (Button) findViewById(R.id.restore_fonts);
+			// button.setVisibility(View.VISIBLE);
+			// if(new File(getBackupFolder()).exists()){
+			// button.setEnabled(true);
+			// }else{
+			// button.setEnabled(false);
+			// }
+			// button.setOnClickListener(this);
+		} else {
 			Log.w(TAG, "sdcard is not present or not mounted");
 			alertUser("sdcard is not present or not mounted", null);
 		}
@@ -317,12 +352,12 @@ DialogInterface.OnClickListener {
 			uninstallThis();
 		} else if (v.getId() == R.id.install_fonts) {
 			alertUser(
-					"This app now mounts /system partions rw and replaces Droid*.ttf fonts in /system/fonts/", 
+					"This app now mounts /system partions rw and replaces Droid*.ttf and optionally Roboto*.ttf (ICS) fonts in /system/fonts/",
 					this);
-		}else if(v.getId() == R.id.restore_fonts){
-			new AsyncTask<Void, Void, Boolean>(){
+		} else if (v.getId() == R.id.restore_fonts) {
+			new AsyncTask<Void, Void, Boolean>() {
 				@Override
-				protected Boolean doInBackground(Void... params){
+				protected Boolean doInBackground(Void... params) {
 					return restore();
 				}
 
@@ -333,11 +368,11 @@ DialogInterface.OnClickListener {
 				}
 
 				@Override
-				protected void onPostExecute(Boolean result){
+				protected void onPostExecute(Boolean result) {
 					Installer.this.enableView();
-					if(result){
+					if (result) {
 						notifyUser("Original Fonts Restored");
-					}else{
+					} else {
 						alertUser("Could Not Restore Fonts", null);
 					}
 					super.onPostExecute(result);
@@ -355,7 +390,7 @@ DialogInterface.OnClickListener {
 		button.setOnClickListener(this);
 		button = (Button) findViewById(R.id.restore_fonts);
 		try {
-			if(new File(getBackupFolder()).exists()){
+			if (new File(getBackupFolder()).exists()) {
 				button.setEnabled(true);
 				button.setOnClickListener(this);
 			}
@@ -363,7 +398,7 @@ DialogInterface.OnClickListener {
 			alertUser(e.getMessage(), null);
 		}
 		ProgressBar pb = (ProgressBar) findViewById(R.id.installing);
-		pb.setVisibility(View.INVISIBLE);	
+		pb.setVisibility(View.INVISIBLE);
 	}
 
 	public void disableView(boolean barView) {
@@ -374,7 +409,7 @@ DialogInterface.OnClickListener {
 		button = (Button) findViewById(R.id.restore_fonts);
 		button.setEnabled(false);
 
-		if(barView){
+		if (barView) {
 			ProgressBar pb = (ProgressBar) findViewById(R.id.installing);
 			pb.setVisibility(View.VISIBLE);
 		}
@@ -403,14 +438,15 @@ DialogInterface.OnClickListener {
 				@Override
 				protected void onPostExecute(Boolean result) {
 					Installer.this.enableView();
-					if(this.isCancelled()){
+					if (this.isCancelled()) {
 						return;
 					}
 					if (result) {
-						notifyUser("Installation Sucsessful\n" +
-						"Reboot the device for changes to take effect!");
+						notifyUser("Installation Sucsessful\n"
+								+ "Reboot the device for changes to take effect!");
 					} else {
-						alertUser("The fonts were not installed. Please check the logs and contact the developer :(",
+						alertUser(
+								"The fonts were not installed. Please check the logs and contact the developer :(",
 								null);
 					}
 					super.onPostExecute(result);
@@ -427,7 +463,6 @@ DialogInterface.OnClickListener {
 	}
 
 	@Override
-
 	public final boolean onOptionsItemSelected(final MenuItem item) {
 		switch (item.getItemId()) {
 		case R.id.send_logs: // start settings activity
@@ -438,17 +473,19 @@ DialogInterface.OnClickListener {
 		}
 	}
 
-	//catches the MEDIA_MOUNT intent
+	// catches the MEDIA_MOUNT intent
 	public class SDCardMountIntentReceiver extends BroadcastReceiver {
 		private Installer installer;
-		public SDCardMountIntentReceiver(Installer installer){
+
+		public SDCardMountIntentReceiver(Installer installer) {
 			this.installer = installer;
 		}
+
 		@Override
 		public void onReceive(Context arg0, Intent intent) {
-			if(intent.getAction().equals(Intent.ACTION_MEDIA_MOUNTED)){
+			if (intent.getAction().equals(Intent.ACTION_MEDIA_MOUNTED)) {
 				installer.enableView();
-			}else if(intent.getAction().equals(Intent.ACTION_MEDIA_EJECT)){
+			} else if (intent.getAction().equals(Intent.ACTION_MEDIA_EJECT)) {
 				installer.disableView(false);
 				alertUser("sdcard has been removed or unmounted", null);
 			}
